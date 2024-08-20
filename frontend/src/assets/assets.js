6229d9db931c07545fe95a7343d02b220f3d6f1f
Mohammed Parves <mdperrfan@gmail.com>
2024-08-21 00:56:39 +0600

    Navbar done

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,8 @@
+import navlogo from "./logo/navlogo.png"
+import search from "./search.png"
+import basket from "./basket.png"
+export const assests = {
+    navlogo,
+    search,
+    basket
+}
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import { assests } from '../../assets/assets'
+const Navbar = () => {
+    const[menu,setMenu]=useState("home")
+    return (
+        <div className='navbar'>
+            <img src={assests.navlogo} alt="Navlogo" />
+            <ul className="navbar-menu">
+                <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+                <li onClick={()=>setMenu("menu")}  className={menu==="menu"?"active":""}>Menu</li>
+                <li onClick={()=>setMenu("contact-us")}  className={menu==="contact-us"?"active":""}>Contact us</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assests.search} alt="search" style={{width:"38px"}} />
+                <div className="navbar-basket">
+                    <img src={assests.basket} alt="basket" style={{width:"40px"}}/>
+                    <div className="dot"></div>
+                </div>
+                <button>Sign in</button>
+            </div>    
+        </div>
+    )
+}
+
+export default Navbar
