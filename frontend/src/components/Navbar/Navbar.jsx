8e9ec5045faba82683dc62750233016676dbf576
Mohammed Parves <mdperrfan@gmail.com>
2024-08-24 21:32:38 +0600

    Cart completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./Navbar.css"
 import { assests } from '../../assets/assets'
 import { Link } from 'react-router-dom'
-const Navbar = ({setShowLogin}) => {
-    const[menu,setMenu]=useState("home")
+import { StoreContext } from '../../context/StoreContext'
+const Navbar = ({ setShowLogin }) => {
+    const [menu, setMenu] = useState("home");
+
+    const {getTotalCartAmount}=useContext(StoreContext)
     return (
         <div className='navbar'>
-            <img className='logo' src={assests.navlogo} alt="Navlogo" />
+            <Link to='/'><img className='logo' src={assests.navlogo} alt="Navlogo" /></Link>
             <ul className="navbar-menu">
-                <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
-                <a href='#menu' onClick={()=>setMenu("menu")}  className={menu==="menu"?"active":""}>Menu</a>
-                <a href='#app-download' onClick={()=>setMenu("about")}  className={menu==="about"?"active":""}>Mobile-app</a>
-                <a href='#contact-us' onClick={()=>setMenu("contact-us")}  className={menu==="contact-us"?"active":""}>Contact us</a>
+                <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+                <a href='#menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+                <a href='#app-download' onClick={() => setMenu("about")} className={menu === "about" ? "active" : ""}>Mobile-app</a>
+                <a href='#contact-us' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact us</a>
             </ul>
             <div className="navbar-right">
-                <img src={assests.search} alt="search" style={{minWidth:"28px",maxWidth:"37px"}} />
+                <img src={assests.search} alt="search" style={{ minWidth: "28px", maxWidth: "37px" }} />
                 <div className="navbar-basket">
-                    <img src={assests.basket} alt="basket" style={{minWidth:"30px",maxWidth:"38px"}}/>
-                    <div className="dot"></div>
+                    <Link to='/cart'>
+                        <img src={assests.basket} alt="basket" style={{ minWidth: "30px", maxWidth: "38px" }} />
+                    </Link>
+                    <div className={getTotalCartAmount()===0?"":"dot"}></div>
                 </div>
-                <button onClick={()=>setShowLogin(true)}>Sign in</button>
-            </div>    
+                <button onClick={() => setShowLogin(true)}>Sign in</button>
+            </div>
         </div>
     )
 }
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null)
 
@@ -14,15 +14,23 @@ export const StoreContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+    const getTotalCartAmount = () => {
+        let totlaAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totlaAmount += itemInfo.price * cartItems[item]
+            }
+        }
+        return totlaAmount;
+    }
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
     return (
         <StoreContext.Provider value={contextValue}>
